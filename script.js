898ce4732e69a1919928dff1dada3fdc38166d0f
shriyashahh <76067960+shriyashahh@users.noreply.github.com>
2026-07-28 15:18:25 +0530

    Implement loading messages and progress bar
    Added a loading sequence with progress bar and messages.

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-document.getElementByld("btn").onclick = function () {
-  alert("It worksss");
-};
+const loadingMessages = [
+"Accessing Memory Archive...",
+"Loading Clash Royale Battles...",
+"Finding Late Night Coversations...",
+"Collecting Food Memories...",
+"Recovering Abe Saale Moments...",
+"Restoring Sid's Playlist...",
+"Almost There..."
+];
+
+let progress = 0;
+let index = 0;
+const bar = document.getElementById("progressBar");
+const text = document.getElementById("loadingText");
+function load(){
+  if(progress<=100){
+    bar.style.width=progress+"%";
+    if(index<loadingMessages.length){
+      text.innerHTML =loadingMessages[index];
+      index++;
+    }
+    progress+=15;
+    setTimeout(load,800);
+  }
+}
+load();
+  
