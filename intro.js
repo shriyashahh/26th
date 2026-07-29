b4712ebf9676fc8edbab8f8376f566e284e217fc
shriyashahh <76067960+shriyashahh@users.noreply.github.com>
2026-07-29 15:10:47 +0530

    Refactor event listeners and styles in intro.js

--- intro.js
+++ intro.js
@@ -1,7 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded",()=>{
 
-    const envelope = document.querySelector(".envelope-container");
+const seal=document.querySelector(".wax-seal");
+const flap=document.querySelector(".envelope-flap");
 
-    envelope.classList.add("show");
+seal.addEventListener("click",()=>{
+
+seal.style.transform="translateX(-50%) scale(0)";
+
+seal.style.opacity="0";
+
+setTimeout(()=>{
+
+flap.style.transform="rotateX(180deg)";
+
+},300);
+
+});
 
 });
