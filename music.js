44473abaeae9d29299e227edbfb5e99e95fe4f92
shriyashahh <76067960+shriyashahh@users.noreply.github.com>
2026-07-30 20:01:21 +0530

    Update music.js

--- music.js
+++ music.js
@@ -1,1 +1,26 @@
+function goBack() {
 
+    window.location.href = "home.html";
+
+}
+
+const songs = [
+
+    "assets/music/song1.mp3",
+    "assets/music/song2.mp3",
+    "assets/music/song3.mp3",
+    "assets/music/song4.mp3",
+    "assets/music/song5.mp3",
+    "assets/music/song6.mp3"
+
+];
+
+function playSong(index) {
+
+    const player = document.getElementById("player");
+
+    player.src = songs[index];
+
+    player.play();
+
+}
