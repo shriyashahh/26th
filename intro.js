2643060bdb66f51627ffd087f5eb123cd2b20c50
shriyashahh <76067960+shriyashahh@users.noreply.github.com>
2026-07-30 11:55:32 +0530

    Implement envelope reveal animation
    Add envelope animation on DOMContentLoaded event.

--- intro.js
+++ intro.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded",()=>{
-
-// Phase 1 only.
-// Later we'll add desk, envelope and sounds here.
-
 });
+const envelope=document.getElementbyId("envelope");
+setTimeout(()=>{
+  envelope.stlye.transition="1.5s ease-in-out";
+  envelope.style.transform-
+    "translate(-50%,-50%)rotateT(180deg)";
+  setTimeout(()=>{
+    envelope.src="assets/envelope_front.jpg";
+    envelope.style.transform=
+      "translate(-50%,-50%)rotateY(0deg)";
+  },750);
+},6500);
+  
+
