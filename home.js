5287e6f921fe691af5b5ae6f7766ed547c2fb984
shriyashahh <76067960+shriyashahh@users.noreply.github.com>
2026-07-30 13:47:09 +0530

    Add unlockDownloads function to show downloads app
    Fix typo in display style for downloadsApp.

--- home.js
+++ home.js
@@ -3,3 +3,10 @@ function openApp(page) {
     window.location.href = page;
 
 }
+
+function unlockDownloads(){
+    document.getElementById("downloadsApp")
+        .style.display="felx";
+}
+
+unlockDownloads();
