95c2071bbefa79dd2f5213617f05314debfdcd90
shriyashahh <76067960+shriyashahh@users.noreply.github.com>
2026-07-30 12:47:01 +0530

    Implement unlock screen functionality
    Add click event to unlock screen and redirect to home.

--- lockscreen.js
+++ lockscreen.js
@@ -1,1 +1,12 @@
+document.body.addEventListener("click", () => {
 
+    document.querySelector(".phone").style.transform =
+        "translateY(-100%)";
+
+    setTimeout(() => {
+
+        window.location.href = "home.html";
+
+    }, 800);
+
+});
