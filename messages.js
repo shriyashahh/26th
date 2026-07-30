e26ccf7e0226ebb6e32d61c281530567c09185c7
shriyashahh <76067960+shriyashahh@users.noreply.github.com>
2026-07-30 20:10:43 +0530

    Remove goBack and playSong functions from messages.js

--- messages.js
+++ messages.js
@@ -1,17 +1,1 @@
-function goBack() {
 
-    window.location.href = "home.html";
-
-}
-
-function playSong(number) {
-
-    const player = document.getElementById("player");
-
-    player.src = `assets/music/song${number}.mp3`;
-
-    player.load();
-
-    player.play();
-
-}
