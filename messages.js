e39ca27b9bf907dd99b496356106fe79a10d96d2
shriyashahh <76067960+shriyashahh@users.noreply.github.com>
2026-07-30 20:10:29 +0530

    Add functions to navigate and play music

--- messages.js
+++ messages.js
@@ -1,1 +1,17 @@
+function goBack() {
 
+    window.location.href = "home.html";
+
+}
+
+function playSong(number) {
+
+    const player = document.getElementById("player");
+
+    player.src = `assets/music/song${number}.mp3`;
+
+    player.load();
+
+    player.play();
+
+}
