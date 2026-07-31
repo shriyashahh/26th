fa3ce259378e665433b22343c355b15027a00f24
shriyashahh <76067960+shriyashahh@users.noreply.github.com>
2026-07-31 16:54:47 +0530

    Update games.js

--- games.js
+++ games.js
@@ -1,20 +1,28 @@
+const arena = document.getElementById("arena");
+
 const battleText = document.getElementById("battleText");
 
 const overlay = document.getElementById("overlay");
+
+const spotlight = document.getElementById("spotlight");
 
 const king = document.getElementById("king");
 
 const speech = document.getElementById("speech");
 
-const goblin = document.getElementById("balloonGoblin");
-
-const goblinBubble = document.getElementById("goblinBubble");
-
 const fanfare = document.getElementById("fanfare");
 
 const kingVoice = document.getElementById("kingVoice");
 
+const run1 = document.getElementById("run1");
+
+const run2 = document.getElementById("run2");
+
 const footsteps = document.getElementById("footsteps");
+
+const giggle = document.getElementById("giggle");
+
+const cheer = document.getElementById("cheer");
 
 setTimeout(() => {
 
@@ -36,23 +44,79 @@ setTimeout(() => {
 
     }, 1200);
 
+}, 3000);
+
+/* AFTER KING FINISHES */
+
+setTimeout(() => {
+
+    battleText.style.opacity = "0";
+
+    speech.style.opacity = "0";
+
+    arena.classList.add("party-mode");
+
+    spotlight.style.opacity = "1";
+
+    king.classList.add("king-top");
+
+    cheer.play();
+
+    startGoblins();
+
+}, 10000);
+
+function startGoblins() {
+
+    document.querySelectorAll(".balloon").forEach(
+
+        (goblin, index) => {
+
+            setTimeout(() => {
+
+                goblin.style.opacity = "1";
+
+                goblin.style.animation =
+                    "rightToLeft 7s linear forwards";
+
+            }, index * 800);
+
+        }
+
+    );
+
+    document.querySelectorAll(".confetti").forEach(
+
+        (goblin, index) => {
+
+            setTimeout(() => {
+
+                goblin.style.opacity = "1";
+
+                goblin.style.animation =
+                    "diagonal 6s linear forwards";
+
+            }, index * 700);
+
+        }
+
+    );
+
     setTimeout(() => {
+
+        run1.play();
 
         footsteps.play();
 
-        goblin.style.opacity = "1";
+        giggle.play();
 
-        goblin.style.animation =
-            "goblinRun 6s linear forwards";
+        const cake = document.getElementById("cakeGoblin");
 
-        goblinBubble.style.opacity = "1";
+        cake.style.opacity = "1";
 
-        setTimeout(() => {
+        cake.style.animation =
+            "leftToRight 8s linear forwards";
 
-            goblinBubble.style.opacity = "0";
+    }, 2200);
 
-        }, 2500);
-
-    }, 5000);
-
-}, 3000);
+}
