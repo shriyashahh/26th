965cf41a0549f110ecf038e673474e9cac272718
shriyashahh <76067960+shriyashahh@users.noreply.github.com>
2026-07-31 14:46:02 +0530

    Update games.js

--- games.js
+++ games.js
@@ -1,20 +1,26 @@
+const battleText = document.getElementById("battleText");
+
 const overlay = document.getElementById("overlay");
 
 const king = document.getElementById("king");
 
 const speech = document.getElementById("speech");
 
+const goblin = document.getElementById("balloonGoblin");
+
+const goblinBubble = document.getElementById("goblinBubble");
+
 const fanfare = document.getElementById("fanfare");
 
 const kingVoice = document.getElementById("kingVoice");
 
+const footsteps = document.getElementById("footsteps");
+
 setTimeout(() => {
 
-    document.getElementById("battleText").innerHTML =
-        "⏸️ BATTLE FROZEN ⏸️";
+    battleText.innerHTML = "⏸️ BATTLE FROZEN ⏸️";
 
-    overlay.style.background =
-        "rgba(0, 0, 0, 0.75)";
+    overlay.style.background = "rgba(0,0,0,0.75)";
 
     fanfare.play();
 
@@ -29,5 +35,24 @@ setTimeout(() => {
         kingVoice.play();
 
     }, 1200);
+
+    setTimeout(() => {
+
+        footsteps.play();
+
+        goblin.style.opacity = "1";
+
+        goblin.style.animation =
+            "goblinRun 6s linear forwards";
+
+        goblinBubble.style.opacity = "1";
+
+        setTimeout(() => {
+
+            goblinBubble.style.opacity = "0";
+
+        }, 2500);
+
+    }, 5000);
 
 }, 3000);
